43f0ec0ff0d2bff35dad35e1738f10b6eab4c68b
eunhong <dmsghd95@naver.com>
2023-08-15 19:23:48 +0900

    chore : modify UserController RequestMapping URI

--- src/main/java/com/eunhong/sns/controller/UserController.java
+++ src/main/java/com/eunhong/sns/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/vi/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor // userService가 final로 선언되어있으므로 자동으로 생성자 만들어서 주입받을 수 있게 함
 public class UserController {
     private final UserService userService;
