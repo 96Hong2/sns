6f7e9c92dd65c7328a21475b6063f90a08cd8157
eunhong <dmsghd95@naver.com>
2023-08-30 21:50:38 +0900

    fix : implement PostControllerTest

--- src/main/java/com/eunhong/sns/exception/ErrorCode.java
+++ src/main/java/com/eunhong/sns/exception/ErrorCode.java
@@ -12,6 +12,8 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"), // 로그인 시 해당 유저 없음
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"), // 로그인 시 패스워드 틀림
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"), // 토큰이 유효하지 않음
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"), // 포스트 수정 시 포스트를 찾을 수 없는 경우
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"), // 포스트 수정 시 작성자가 아닌 경우
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
--- src/test/java/com/eunhong/sns/controller/PostControllerTest.java
+++ src/test/java/com/eunhong/sns/controller/PostControllerTest.java
@@ -1,7 +1,9 @@
 package com.eunhong.sns.controller;
 
 import com.eunhong.sns.controller.request.PostCreateRequest;
-import com.eunhong.sns.controller.request.UserJoinRequest;
+import com.eunhong.sns.controller.request.PostModifyRequest;
+import com.eunhong.sns.exception.ErrorCode;
+import com.eunhong.sns.exception.SnsApplicationException;
 import com.eunhong.sns.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -14,7 +16,11 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,4 +63,68 @@ public class PostControllerTest {
                 ).andDo(print())
                 .andExpect(status().isUnauthorized());
     }
+
+    @Test
+    @WithMockUser // 포스트 수정 시 인증된 유저가 작성한 것을 표현
+    void 포스트수정() throws Exception { // 포스트 수정이 성공한 경우
+
+        String title = "title";
+        String body = "body";
+
+        mockMvc.perform(put("/api/v1/posts/1") // posts 다음에 수정할 포스트의 아이디를 넣었음
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser // 포스트 수정 시 인증되지 않은 유저가 작성한 것을 표현
+    void 포스트수정시_로그인하지않은경우() throws Exception {
+
+        String title = "title";
+        String body = "body";
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+
+        String title = "title";
+        String body = "body";
+
+        // any()를 썼기 때문에 eq로 감쌈
+        // modify를 했을 때 INVALID_PERMISSION 에러를 발생시키는지 확인
+        doThrow(new SnsApplicationException(ErrorCode.INVALID_PERMISSION)).when(postService).modify(eq(title), eq(body), any(), eq(1));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_수정하려는_글이_없는경우_에러발생() throws Exception {
+
+        String title = "title";
+        String body = "body";
+
+        // modify를 했을 때 POST_NOT_FOUND 에러를 발생시키는지 확인
+        doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).modify(eq(title), eq(body), any(), eq(1));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
 }
