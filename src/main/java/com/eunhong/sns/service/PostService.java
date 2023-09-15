d79755f51e35b594850186e6891aafbaaefe4bb9
eunhong <dmsghd95@naver.com>
2023-09-15 20:21:00 +0900

    feat : add like Controller Test

--- src/main/java/com/eunhong/sns/service/PostService.java
+++ src/main/java/com/eunhong/sns/service/PostService.java
@@ -86,4 +86,9 @@ public class PostService {
 
         return postEntityRepository.findAllByUser(userEntity, pageable).map(Post::fromEntity);
     }
+
+    @Transactional
+    public void like(Integer postId, String userName) {
+
+    }
 }
